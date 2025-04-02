0294b53458cf4f44b7e2dc4f9a72f0c24d0329a2
himanshu.chavda <himanshu.chavda@rapidops.co>
2025-04-02 19:16:04 +0530

    address model is done

---
+++ src/models/address.model.js
@@ -0,0 +1,45 @@
+
+import { model, Schema } from 'mongoose';
+
+const addressSchema = new Schema(
+  {
+    address_line1: {
+      type: String,
+      required: [true, 'Address Line 1 is required'],
+    },
+    city: {
+      type: String,
+      required: [true, 'City is required'],
+    },
+    state: {
+      type: String,
+      required: [true, 'State is required'],
+    },
+    pincode: {
+      type: String,
+      required: [true, 'Pincode is required'],
+    },
+    country: {
+      type: String,
+      default: 'India',
+    },
+    mobile: {
+      type: String,
+      required: [true, 'Mobile number is required'],
+    },
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Address = model('Address', addressSchema);
+export default Address;
+    
