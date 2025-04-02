1170779673dc084b43267c39a3af843422052655
himanshu.chavda <himanshu.chavda@rapidops.co>
2025-04-02 19:17:01 +0530

    order model is done

---
+++ src/models/order.model.js
@@ -0,0 +1,61 @@
+import { model, Schema } from 'mongoose';
+
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    orderId: {
+      type: String,
+      required: [true, 'Provide Order ID!'],
+      unique: true,
+    },
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        priceAtPurchase: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    paymentId: {
+      type: String,
+      default: '',
+    },
+    payment_status: {
+      type: String,
+      enum: ['Pending', 'Completed', 'Failed'],
+      default: 'Pending',
+    },
+    delivery_address: {
+      type: Schema.Types.ObjectId,
+      ref: 'Address',
+      required: true,
+    },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+      default: 'Pending',
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = model('Order', orderSchema);
+export default Order;
